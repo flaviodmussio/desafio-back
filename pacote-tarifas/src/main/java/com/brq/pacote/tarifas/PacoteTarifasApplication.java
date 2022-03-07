9f89c44a2512a0dff13767bc40ad4edaecad7858
MarcosLorencini <mlorencinif@gmail.com>
2022-03-07 19:24:33 -0300

    Projeto PacoteTarifa criado

---
+++ pacote-tarifas/src/main/java/com/brq/pacote/tarifas/PacoteTarifasApplication.java
@@ -0,0 +1,13 @@
+package com.brq.pacote.tarifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacoteTarifasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacoteTarifasApplication.class, args);
+	}
+
+}
---
+++ pacote-tarifas/src/test/java/com/brq/pacote/tarifas/PacoteTarifasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brq.pacote.tarifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacoteTarifasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
